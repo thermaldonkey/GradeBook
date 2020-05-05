b9ba28ad780e05198c9c568c51c5aa880d9be34f
thermaldonkey <bradley_rice@mac.com>
2020-05-04 19:52:55 -0500

    init commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string V = "foo";
+            string name;
+            if(args.Length > 0) {
+                name = args[0];
+            } else {
+                name = V;
+            }
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
